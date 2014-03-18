012db7bb5c7d8d0cbaacf5d0f2850112a596e3fd
Vlad <v-eliseev@yandex.ru>
2014-03-19 02:23:47 +0400

    plugin update

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -54,7 +54,7 @@ grails.project.dependency.resolution = {
         }
 
         compile ":webxml:1.4.1"
-        compile ":asset-pipeline:1.6.4"
-        compile ":less-asset-pipeline:1.5.4"  // DO NOT CHANGE!!!
+        compile ":asset-pipeline:1.7.0"
+        compile ":less-asset-pipeline:1.7.0"
     }
 }
