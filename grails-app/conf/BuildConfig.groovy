7a5447565dbb2e9cbab03265573836120cde66d2
Vlad <v-eliseev@yandex.ru>
2014-04-24 23:05:49 +0400

    upgrade to grails 2.3.8

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -54,7 +54,7 @@ grails.project.dependency.resolution = {
         }
 
         compile ":webxml:1.4.1"
-        compile ":asset-pipeline:1.8.1"
+        compile ":asset-pipeline:1.8.3"
         compile ":less-asset-pipeline:1.7.0"
     }
 }
